a81de3970ceb4100711308ebe69b92ef1cb793c9
jezozwiesh <oli06.om@gmail.com>
2024-03-11 13:14:25 +0100

    Add board browser and creator

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,27 +4,35 @@ const { Hash_Password } = require('../lib/hash_utilities');
 const { is_auth, if_exists } = require('../lib/middlewares/auth_check');
 const User = require('../models/user');
 const Task = require('../models/task');
+const Board = require('../models/board');
 
 /////////////////////////////////////////////////////////////////////
 
-router.post('/login', passport.authenticate('local', {failureRedirect: '/fail', successRedirect: '/board'}), (req, res) => {});
+router.post('/login', passport.authenticate('local', {failureRedirect: '/fail', successRedirect: '/boards'}), (req, res) => {});
 
 router.post('/register', if_exists, (req, res) => {
     const password = req.body.reg_password.trim();
     const username = req.body.reg_username.trim();
 
     const salt_hash = Hash_Password(password);
-
-    const new_user = new User({
+    
+    new User({
         username: username,
         hash: salt_hash.hash,
         salt: salt_hash.salt
-    });
-
-    new_user.save()
+    })
+    .save()
     .then(user => console.log('Nowy uzytkownik: ' + user));
 
     res.redirect('/sign-user');   
+});
+
+router.post('/board-create', (req, res) => {
+    new Board({
+        name: req.body.boardName,
+        creatorId: req.user._id,
+        creation_date: Date.now()
+    }).save();
 });
 
 router.post('/board', (req, res) => {
@@ -35,6 +43,7 @@ router.post('/board', (req, res) => {
         creation_date: Date.now(),
         end_date: req.body.taskDeadline,
         // assigned_userId: req.body.user,
+        category: req.body.taskCategory,
         creatorId: req.user._id
     });
 
@@ -51,14 +60,17 @@ router.get('/sign-user', (req, res) => {
     res.render('account-form');
 });
 
-router.get('/success', (req, res) => 
-{
-    res.send('success');
-});
-
 router.get('/fail', (req, res) =>
 {
     res.send('fail');
+});
+
+router.get('/boards', is_auth, async (req, res) => {
+    const data = {
+        username: req.user.username,
+        user_boards: await Board.find({creatorId: req.user._id}).exec()
+    };
+    res.render('board-browser', {data});
 });
 
 router.get('/board', is_auth, (req, res) => {
