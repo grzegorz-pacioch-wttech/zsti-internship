94c838cc559cac31a97b30b53be3611b52bdbea8
jezozwiesh <oli06.om@gmail.com>
2024-03-14 11:07:09 +0100

    fix: env variable name

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 
 require('dotenv').config();
 
-mongoose.connect(process.env.db_url, {dbName: 'kanban'})
+mongoose.connect(process.env.DB_URL, {dbName: 'kanban'})
 .then(() => console.log('[Server] Connected to the database'))
 // .catch(err => console.log(err));
 
