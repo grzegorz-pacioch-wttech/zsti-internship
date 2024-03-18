8d1bf72460df4391f7b937aa1b7bd292f98d9cc8
jezozwiesh <oli06.om@gmail.com>
2024-03-18 11:02:23 +0100

    feat: load column names from board database entry

--- src/models/board.js
+++ src/models/board.js
@@ -6,7 +6,12 @@ const Board = mongoose.model('Board',
         name : {type: String, required: true},
         creatorId : {type: ObjectId, required: true},
         collab_usersId : {type: ObjectId, required: false},
-        creation_date : {type: Date, required: true}
+        creation_date : {type: Date, required: true},
+        column1_name : {type: String, required: true, default: 'Backlog'},
+        column2_name : {type: String, required: true, default: 'In progress'},
+        column21_name : {type: String, required: true, default: 'Waiting'},
+        column22_name : {type: String, required: true, default: 'Working'},
+        column3_name : {type: String, required: true, default: 'Done'},
     })
 );
 module.exports = Board;
